8dbaa1d96fa11929e43569aeb4d6bd9ed3f48bf0
noahbrase1 <noahgbrase@gmail.com>
2025-07-25 10:50:58 -0600

    commit boxes

---
+++ anagramsVue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+      <div class="box1"></div>
+      <div class="box2"></div>
+      <div class="box3"></div>
+      <div class="box4"></div>
+      <div class="box5"></div>
+      <div class="box6"></div>
+  </div>
+</template>
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;   /* horizontal center */
+    align-items: center;       /* vertical center */
+    height: 100vh;             /* make body fill the viewport height */
+    margin: 0;                 /* remove default margin for full control */
+  }
+
+  .container {
+    display: flex;
+    justify-content: center; /* or space-between */
+    flex-wrap: wrap;
+    width: 80vw;
+    margin: 0 auto;
+    gap: 1vw;
+  }
+
+  .box1, .box2, .box3, .box4, .box5, .box6 {
+    width: 5vw;
+    height: 5vw;
+    background-color: white;
+    border: 0.5vw solid black;
+  }
+
+
+</style>
