0be4be70e424555d83122156ac5a79d2d84b847e
noahbrase1 <noahgbrase@gmail.com>
2025-07-25 18:05:13 -0600

    random letters

--- anagramsVue/src/App.vue
+++ anagramsVue/src/App.vue
@@ -7,6 +7,9 @@
       <div class="box5" id="box5">{{ box5 }}</div>
       <div class="box6" id="box6">{{ box6 }}</div>
       <div class="box7" id="box7">{{ box7 }}</div>
+      <div class="timer-wrapper">
+        <div class="timer"> {{ timeLeft }} </div>
+      </div>
       <div>
         <button class="startButton" @click="start">Start</button>
       </div>
@@ -34,7 +37,7 @@
     flex-wrap: wrap;
     width: 80vw;
     margin: 0 auto;
-    gap: 2vw;
+    gap: 0vw;
   }
 
   .box1, .box2, .box3, .box4, .box5, .box6, .box7 {
@@ -45,6 +48,7 @@
     background-color: white;
     border: 0.5vw solid black;
     border-radius: 1vw;
+    margin: 0.5vw;
   }
 
   .box1, .box2, .box3, .box4, .box5, .box6, .box7 {
@@ -53,6 +57,19 @@
     line-height: max(4vw, 25px); /* vertically centers single-character text */
     font-weight: bold;
     color: black;
+  }
+
+  .timer-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 0.1vw; /* or even 0.3vw if needed */
+    margin-bottom: 1vw;
+  }
+
+  .timer {
+    font-size: max(5vw, 30px);
+    font-weight: bold;
   }
 
   .container > div:last-child {
@@ -70,6 +87,7 @@
     border-radius: 1vw;
     cursor: pointer;
   }
+
 </style>
 
 <script setup>
@@ -94,6 +112,10 @@
   const box6 = ref('')
   const box7 = ref('')
 
+  const timeLeft = ref(30)
+  const timerActive = ref(false)
+  let timer = null
+
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
@@ -107,7 +129,19 @@
     box5.value = vowels[getRandomInt(0, vowels.length - 1)]
     box6.value = letters[getRandomInt(0, letters.length - 1)]
     box7.value = either[getRandomInt(0, either.length - 1)]
-  }
 
+    if (timer) clearInterval(timer)
+      timeLeft.value = 30
+      timerActive.value = true
+
+      timer = setInterval(() => {
+        if (timeLeft.value > 0) {
+          timeLeft.value--
+        } else {
+          clearInterval(timer)
+          timerActive.value = false
+        }
+      }, 1000)
+  }
 
 </script>
